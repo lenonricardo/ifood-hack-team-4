81e1ad7c3a9e51341c03cb9cd2dea6b11381a662
lenonricardo <blcrz@outlook.com>
2022-02-19 02:24:25 -0300

    add Dialog component

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,56 +1,64 @@
 <template>
-  <v-card
-    :loading="loading"
-    class="card"
-    max-width="374"
-    rounded
-    elevation="10"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <div>
+    <v-card
+      class="card"
+      max-width="374"
+      rounded
+      elevation="10"
+    >
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
 
-    <v-card-title class="card-title">
-      <span>{{ title }}</span>
-      <span>{{ label }}</span>
-    </v-card-title>
+      <v-card-title class="card-title">
+        <span>{{ title }}</span>
+        <span>{{ label }}</span>
+      </v-card-title>
 
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+      <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      ></v-img>
 
-    <v-card-title class="card-title pb-0">
-      <span>Price: {{ price }}</span>
-    </v-card-title>
+      <v-card-title class="card-title pb-0">
+        <span>Price: {{ price }}</span>
+      </v-card-title>
 
-    <div class="card-desc">
-      <v-card-text>
-        {{ desc }}
-      </v-card-text>
+      <div class="card-desc">
+        <v-card-text>
+          {{ desc }}
+        </v-card-text>
 
-      <v-card-actions>
-        <v-btn
-          color="#303030"
-          class="mb-0 mr-2 text-capitalize"
-          depressed
-          dark
-          rounded
-          @click="reserve"
-        >
-          Cook
-        </v-btn>
-      </v-card-actions>
-    </div>
-  </v-card>
+        <v-card-actions>
+          <v-btn
+            color="#303030"
+            class="mb-0 mr-2 text-capitalize"
+            depressed
+            dark
+            rounded
+            @click="reserve"
+          >
+            Cook
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </v-card>
+    <Dialog :dialog="dialog" @close="dialog = false"/>
+  </div>
 </template>
 <script>
+  import Dialog from '@/components/Dialog.vue'
+
   export default {
     name: 'Card',
+
+    components: {
+      Dialog
+    },
 
     props: {
       title: {
@@ -70,13 +78,12 @@
     data: () => ({
       loading: false,
       selection: 1,
+      dialog: false
     }),
 
     methods: {
       reserve () {
-        this.loading = true
-
-        setTimeout(() => (this.loading = false), 2000)
+        this.dialog = true
       },
     },
   }
---
+++ frontend/src/components/Dialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Header
+        </v-card-title>
+
+        <slot>
+        </slot>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="$emit('close')"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Dialog',
+
+    props: {
+      dialog: {
+        default: false,
+      }
+    },
+
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
+
+    methods: {
+    },
+  }
+</script>
