f39dc1d307dd7c80d197608b6ae1708bf8b9de82
lenonricardo <blcrz@outlook.com>
2022-02-19 00:47:35 -0300

    fix: add gitgnore

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from '../components/Dashboard.vue'
-import Author from '../components/Author.vue'
-import Book from '../components/Book.vue'
-import Member from '../components/Member.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/author',
-    name: 'Author',
-    component: Author
-  },
-  {
-    path: '/book',
-    name: 'Book',
-    component: Book
-  },
-  {
-    path: '/member',
-    name: 'Member',
-    component: Member
+    name: 'Home',
+    component: Home
   }
 ]
 
